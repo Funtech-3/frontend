b7c7cac3200e017d23c51e9ab9c3f6cca4543540
Leyla <lejla.eyvazova@gmail.com>
2024-04-08 21:08:00 +0700

    feat: add ui kit

---
+++ src/UI Kit/Toast/Toast.tsx
@@ -0,0 +1,11 @@
+import Alert from '@mui/material/Alert';
+import Stack from '@mui/material/Stack';
+
+export default function BasicAlerts() {
+    return (
+        <Stack sx={{ width: '100%' }} spacing={2}>
+            <Alert severity="success">Регистрация отменена</Alert>
+            <Alert severity="error">Ошибка сохранения</Alert>
+        </Stack>
+    );
+}
