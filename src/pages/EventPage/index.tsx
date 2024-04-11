a8dabec3ea31c78ac65aff4fa830084e82735d0d
Leyla <lejla.eyvazova@gmail.com>
2024-04-11 16:40:56 +0700

    feat: add about_section in eventPage

--- src/pages/EventPage/index.tsx
+++ src/pages/EventPage/index.tsx
@@ -1,15 +1,30 @@
 import { Typography } from '@mui/material';
-import Header from '../../components/Header/Header';
-import Footer from '../../components/Footer/Footer';
+import Button from '@mui/material/Button';
+import { Link } from 'react-router-dom';
+import heart from '../../images/heart.svg';
+import './index.scss';
 
 export default function EventPage() {
   return (
     <Typography variant="h1">
-      <Header />
-      <>
-        <section className="about__section"></section>
-      </>
-      <Footer />
+      <main className="EventPage">
+        <section className="about__section">
+          <Link to="/main" className="about__link">
+            Назад
+          </Link>
+          <button>Программирование</button>
+          <button>Митап</button>
+          <h1>About:сloud — infrastructure</h1>
+          <div className="about__paragraph">
+            <p className="about__text1">4 апреля 2024, 16:00 </p>
+            <p className="about__text2">Москва, Экстрополис</p>
+          </div>
+          <Button className="about__button">Зарегистрироваться</Button>
+          <Button className="about__like-button">
+            <img src={heart} alt="like" />
+          </Button>
+        </section>
+      </main>
     </Typography>
   );
 }
