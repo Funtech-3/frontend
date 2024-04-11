126708fcaf11801f8e21a43f41720170554496dc
Constantine <asbest911@gmail.com>
2024-04-11 20:53:16 +0300

    fix: fix header footer styles

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,11 @@
 import logo from '../../assets/images/logo.svg';
 import avatar from '../../assets/images/avatar.webp';
 
-import TextFields from '../../ui-kit/CustomTextField/CustomTextField';
-
 import styles from './styles.module.scss';
 import { Link } from 'react-router-dom';
-// попозже надо починить верстку немного
+import { InputAdornment, TextField, Typography } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import EventIcon from '@mui/icons-material/Event';
 
 const Header = () => {
   return (
@@ -15,12 +15,38 @@ const Header = () => {
           <img src={logo} alt="Лого" />
         </Link>
       </div>
-      <div className={styles.textFields}>
-        <TextFields />
-      </div>
-      <div className={styles.myProfile}>
-        <span className={styles.textProfile}>Мои события</span>
-        <img className={styles.avatar} src={avatar} alt="Аватар" />
+
+      <TextField
+        placeholder="Поиск по названию"
+        size="small"
+        sx={{
+          width: '400px',
+
+          '& .MuiInputBase-root': {
+            backgroundColor: 'rgba(244, 246, 249, 1)',
+            borderRadius: '40px',
+            boxShadow: '0px 2px 8px rgba(0, 20, 51, 0.15)',
+          },
+        }}
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <SearchIcon />
+            </InputAdornment>
+          ),
+        }}
+      />
+
+      <div className={styles.profile}>
+        <Link to="/my-events" className={styles.link}>
+          <EventIcon />
+          <Typography sx={{ fontSize: '18px', fontWeight: '500' }}>
+            Мои события
+          </Typography>
+        </Link>
+        <Link to="/profile" className={styles.link}>
+          <img className={styles.avatar} src={avatar} alt="Аватар" />{' '}
+        </Link>
       </div>
     </header>
   );
