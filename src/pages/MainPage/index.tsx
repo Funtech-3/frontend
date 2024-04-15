0e3138173c1bcc354a0271e7c0d264bde4bc142a
Constantine <asbest911@gmail.com>
2024-04-15 11:18:05 +0300

    fix: fix auth

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Card from '../../components/Card';
 import Filters from '../../components/Filters';
 import { useActions } from '../../hooks/actions';
@@ -16,7 +16,7 @@ import { useGetYaUserInfoQuery } from '../../store/funtech/ya.api';
 export default function MainPage() {
   const { limit, offset, ...filters } = useAppSelector(state => state.filters);
   const { setLimit, setUser } = useActions();
-  const oathToken = localStorage.getItem('authToken');
+  const [token] = useState(localStorage.getItem('authToken') || '');
 
   const { data: events } = useGetEventsQuery({
     limit: limit,
@@ -28,12 +28,13 @@ export default function MainPage() {
   const [postData] = usePostYaUserInfoMutation();
 
   useEffect(() => {
-    if (oathToken) {
+    if (token !== '') {
       refetch();
     }
-  }, [oathToken]);
+  }, [token]);
 
   useEffect(() => {
+    if (!data) return;
     postData(data)
       .unwrap()
       .then(res => setUser(res));
