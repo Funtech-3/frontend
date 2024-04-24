9cc46b55a9e4f5976165ace3dc6e56f79fadb9a7
Constantine <asbest911@gmail.com>
2024-04-24 21:30:02 +0300

    fix: fix errors and delete images

--- src/components/ProgrammList/index.tsx
+++ src/components/ProgrammList/index.tsx
@@ -15,7 +15,7 @@ export default function ProgrammList({
   const [expanded, setExpanded] = React.useState<string | false>(false);
 
   const handleChange =
-    (panel: string) => (event: React.SyntheticEvent, isExpanded: boolean) => {
+    (panel: string) => (_event: React.SyntheticEvent, isExpanded: boolean) => {
       setExpanded(isExpanded ? panel : false);
     };
 
